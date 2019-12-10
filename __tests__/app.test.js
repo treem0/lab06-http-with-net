16ec811650732e5435127aee26105235850c357a
Travis Molter <travismolter@gmail.com>
2019-12-09 17:32:33 -0800

    GET route test is working for /

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+  it('returns hi with GET at the path /', () => {
+    return request(app)
+      .get('/')
+      .then(response => {
+        expect(response.text).toEqual('Hi');
+        expect(response.status).toEqual(200);
+      });
+  });
 });
